a8e75d0a801b648fbd65e51eef29b1b1eb134143
ar-vin-1n0 <aravindrks1912@gmail.com>
2026-06-29 01:12:38 +0530

    done

---
+++ python_projects/terminal_multipurpose_suite/core/config.py
@@ -0,0 +1,24 @@
+APP_NAME = "Terminal Productivity Suite"
+
+VERSION = "1.0.0"
+
+AUTHOR = "Aravind"
+
+DEBUG = True
+
+DATABASE_FOLDER = "database"
+
+DATABASE_NAME = "productivity.db"
+
+DEFAULT_PRIORITY = "Medium"
+
+MAX_TITLE_LENGTH = 100
+
+DATE_FORMAT = "%d-%m-%Y"
+
+TIME_FORMAT = "%H:%M"
+
+MENU_WIDTH = 50
+
+SEPARATOR = "=" * MENU_WIDTH
+
---
+++ python_projects/terminal_multipurpose_suite/core/database.py
@@ -0,0 +1,5 @@
+
+
+class DatsBaseManager:
+    def __init__(self,config):
+        self.config = config
